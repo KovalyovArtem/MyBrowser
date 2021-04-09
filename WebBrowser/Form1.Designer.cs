9aee8a2976ba2415e1bd1dee340f8aedd5c134d8
KovalyovArtem <70099470+KovalyovArtem@users.noreply.github.com>
2021-04-09 16:07:03 +0300

    My browser C# WinForm

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnGoForward = new System.Windows.Forms.Button();
+            this.btnGoBack = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 423);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbxSearch.Location = new System.Drawing.Point(90, 0);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(641, 26);
+            this.tbxSearch.TabIndex = 1;
+            this.tbxSearch.Text = "http://google.com";
+            this.tbxSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxSearch_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSearch.Location = new System.Drawing.Point(731, 0);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(69, 27);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Поиск";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbxSearch);
+            this.panel1.Controls.Add(this.btnReload);
+            this.panel1.Controls.Add(this.btnGoForward);
+            this.panel1.Controls.Add(this.btnGoBack);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 27);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnReload
+            // 
+            this.btnReload.BackgroundImage = global::WebBrowser.Properties.Resources.reload;
+            this.btnReload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnReload.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnReload.FlatAppearance.BorderSize = 0;
+            this.btnReload.Location = new System.Drawing.Point(59, 0);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(31, 27);
+            this.btnReload.TabIndex = 5;
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnGoForward
+            // 
+            this.btnGoForward.BackgroundImage = global::WebBrowser.Properties.Resources.arrow_right;
+            this.btnGoForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGoForward.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnGoForward.FlatAppearance.BorderSize = 0;
+            this.btnGoForward.Location = new System.Drawing.Point(28, 0);
+            this.btnGoForward.Name = "btnGoForward";
+            this.btnGoForward.Size = new System.Drawing.Size(31, 27);
+            this.btnGoForward.TabIndex = 4;
+            this.btnGoForward.UseVisualStyleBackColor = true;
+            this.btnGoForward.Click += new System.EventHandler(this.btnGoForward_Click);
+            // 
+            // btnGoBack
+            // 
+            this.btnGoBack.BackgroundImage = global::WebBrowser.Properties.Resources.arrow_left;
+            this.btnGoBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGoBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnGoBack.FlatAppearance.BorderSize = 0;
+            this.btnGoBack.Location = new System.Drawing.Point(0, 0);
+            this.btnGoBack.Name = "btnGoBack";
+            this.btnGoBack.Size = new System.Drawing.Size(28, 27);
+            this.btnGoBack.TabIndex = 5;
+            this.btnGoBack.UseVisualStyleBackColor = true;
+            this.btnGoBack.Click += new System.EventHandler(this.btnGoBack_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyBrowser";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGoBack;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnGoForward;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            webBrowser1.Url = new Uri("http://google.com");
+        }
+
+        void Proverka()
+        {
+            if (webBrowser1.CanGoForward == false)
+            {
+                btnGoForward.Enabled = false;
+                btnGoForward.BackgroundImage = Properties.Resources.arrow_right_unActive;
+            }
+            else //  if (webBrowser1.CanGoForward == true)
+            {
+                btnGoForward.Enabled = true;
+                btnGoForward.BackgroundImage = Properties.Resources.arrow_right;
+            }
+
+            if (webBrowser1.CanGoBack == false)
+            {
+                btnGoBack.Enabled = false;
+                btnGoBack.BackgroundImage = Properties.Resources.arrow_left_unActive;
+            }
+            else //  if (webBrowser1.CanGoBack == true)
+            {
+                btnGoBack.Enabled = true;
+                btnGoBack.BackgroundImage = Properties.Resources.arrow_left;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(tbxSearch.Text);
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void btnGoForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void tbxSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSearch_Click(sender, e);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+                btnReload_Click(sender, e);
+            if (e.KeyCode == Keys.XButton1)
+                btnGoForward_Click(sender, e);
+            if (e.KeyCode == Keys.XButton2)
+                btnGoBack_Click(sender, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Proverka();
+        }
+    }
+}
